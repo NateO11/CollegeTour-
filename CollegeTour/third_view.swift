3276e56992d5b0e7dd0f1938709984e12341d0be
NateO11 <ndo1503@gmail.com>
2023-11-12 16:13:59 -0500

    add 3rd file , change background to red

---
+++ CollegeTour/third_view.swift
@@ -0,0 +1,20 @@
+//
+//  third_view.swift
+//  CollegeTour
+//
+//  Created by Nate Owen on 11/12/23.
+//
+
+import SwiftUI
+
+struct third_view: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct third_view_Previews: PreviewProvider {
+    static var previews: some View {
+        third_view()
+    }
+}
