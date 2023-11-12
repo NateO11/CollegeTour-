e62efa6c501bd214cb352638cefa45bfb94889a2
NateO11 <ndo1503@gmail.com>
2023-11-12 15:37:32 -0500

    Initial Commit

---
+++ CollegeTour/CollegeTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollegeTourApp.swift
+//  CollegeTour
+//
+//  Created by Nate Owen on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CollegeTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
